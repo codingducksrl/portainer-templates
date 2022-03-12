69383c159d3f7d538947b7267a20dece30912450
Guglielmo Frati <guglielmo.frati@retefrati.it>
2022-03-12 19:24:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 const DIST = "dist";
-const GITHUB_REPO_URL = "https://github.com/codingducksrl/dockerimages";
-const GITHUB_BRANCH = "master";
+const GITHUB_REPO_URL = "https://github.com/codingducksrl/portainer-templates";
+const GITHUB_BRANCH = "main";
 
 build();
 
